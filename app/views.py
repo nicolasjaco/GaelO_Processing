c23926d431799bcea19363bcc0ab1efbff26e47c
Nicolas <nico.jacomi@gmail.com>
2021-04-20 10:06:54 +0200

    Mise en place

--- app/views.py
+++ app/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Je fonctionne")
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
