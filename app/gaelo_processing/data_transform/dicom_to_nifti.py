dc0452ca49f0dc5458944decaea3eee9bc894749
Nicolas <nicolas.jacomi@etud.univ-jfc.fr>
2021-05-18 12:05:45 +0200

    add imageio

--- app/gaelo_processing/data_transform/dicom_to_nifti.py
+++ app/gaelo_processing/data_transform/dicom_to_nifti.py
@@ -2,11 +2,12 @@ from library_dicom.dicom_processor.model.Series import Series
 from ..utips.utips import Utips
 from django.conf import settings
 import tempfile
+
+
 class Dicom_To_Nifti:       
         def to_nifti(folder_path):
-                path=folder_path
-                instance_dicom=Series()
+                path=folder_path               
                 nifti_temp=tempfile.mkdtemp(prefix='nifti_temp')
-                instance_dicom.__init__(path)
-                instance_dicom.export_nifti(file_path=nifti_temp,mode='pet',mask=None)
+                Series.__init__(path)
+                Series.export_nifti(file_path=nifti_temp,mode='pet',mask=None)
                 # shutil.rmtree(path)
