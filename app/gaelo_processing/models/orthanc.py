87b659f9a24a89f1c18e8454787c67ef3e268e43
Nicolas <nicolas.jacomi@etud.univ-jfc.fr>
2021-05-31 17:11:00 +0200

    test

--- app/gaelo_processing/models/orthanc.py
+++ app/gaelo_processing/models/orthanc.py
@@ -1,10 +1,12 @@
 import requests
 import tempfile
 
+from django.conf import settings
+
 class Orthanc:
 
     def get_zip_from_orthanc(self, orthanc_series_id):
-        file=requests.get('http://localhost:8042/series/'+orthanc_series_id+'/archive',auth=('salim','salim'))
+        file=requests.get('http://localhost:8042/series/'+orthanc_series_id+'/archive',auth=(settings.ORTHANC_USERNAME,settings.ORTHANC_PASSWORD))
         content=file.content
         destination=tempfile.mkdtemp(prefix='orthanc_zip_')
         zip_file = open(destination+'/orthanc.zip', 'wb')
