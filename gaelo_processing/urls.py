b6c586a752d3b3fe4c2d0648606c84049d02c501
Nicolas <nicolas.jacomi@etud.univ-jfc.fr>
2021-06-10 19:53:22 +0200

    dicom-to-cnn add

--- gaelo_processing/urls.py
+++ gaelo_processing/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_swagger.views import get_swagger_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view=get_swagger_view(title='gaelo_processing')
+
 # schema_view = get_schema_view(
 #    openapi.Info(
-#       title="Snippets API",
+#       title="gaelo_processing"))
 #       default_version='v1',
 #       description="Test description",
 #       terms_of_service="../Documentation/swagger.yml",
@@ -33,7 +32,7 @@ schema_view=get_swagger_view(title='gaelo_processing')
 urlpatterns = [
     path('app/', include('app.urls')),
     path('admin/', admin.site.urls),
-    path('', schema_view)
+    # path('swagger/', schema_view),
     # path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
